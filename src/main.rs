01cb9036d71e19ccaa27f5c49afaae181bdda50c
BerzanXYZ <berzan@tutanota.com>
2022-06-17 14:51:39 +0300

    Add some file data into static_files module

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 mod utils;
+mod static_files;
 use utils::get_work_dir;
 
 fn main() {
+    // Get and assign work directory
+    // If an error occurs, stop the app
+    let work_dir = match get_work_dir() {
+        Some(wd) => wd,
+        None => {println!("❌ An error occured while reading work directory..."); return;},
+    }; 
+    
     println!();
     println!("🌊 Creta, build TypeScript apps...");
+    println!("Work Directory is {}", work_dir);
+    println!("{}", crate::static_files::PACKAGE_JSON);
 }
