ddaca4a270a86928f688893e694f682ba412d613
BerzanXYZ <berzan@tutanota.com>
2022-06-17 16:10:46 +0300

    Add file creation support

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
             // Convert the project name to lowercase to respect NPM naming rules
             Some(pn) => pn.to_lowercase(),
             None => {println!("🌊 An error occured while setting project name..."); return;},
-        };
+        }
     } else {
         // If the user didn't set a project name
         // Tell user what to do, then stop the app
@@ -36,5 +36,42 @@ fn main() {
     println!();
     println!("🌊 Creta builds your app...");
 
+    // Create the main folder and src folder
+    // If an error occurs stop the app
+    match std::fs::create_dir_all(format!("{}/src", project_dir)) {
+        Ok(_) => (),
+        Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
+    }
+    
+    // Create and write the files
+    match std::fs::write(format!("{}/package.json", project_dir), static_files::PACKAGE_JSON) {
+        Ok(_) => (),
+        Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
+    }
+    match std::fs::write(format!("{}/tsconfig.json", project_dir), static_files::TSCONFIG_JSON) {
+        Ok(_) => (),
+        Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
+    }
+    match std::fs::write(format!("{}/.gitignore", project_dir), static_files::_GITIGNORE) {
+        Ok(_) => (),
+        Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
+    }
+    match std::fs::write(format!("{}/src/index.ts", project_dir), static_files::SRC_INDEX_TS) {
+        Ok(_) => (),
+        Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
+    }
+
+    // Print success message
+    println!();
+    println!("All done!");
+    println!();
+    println!("To prepare your app, run the commands below:");
+    println!("  cd {}", project_name);
+    println!("  yarn install");
+    println!();
+    println!("To start your app, run the commands below:");
+    println!("  yarn start");
+    println!();
+    println!("🌊 By Creta");
 
 }
