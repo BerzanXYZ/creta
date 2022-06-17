091bdd2502813d1d20b60e3edc8aeee8fe4b3076
BerzanXYZ <berzan@tutanota.com>
2022-06-17 15:18:31 +0300

    Add index.ts data using the renamed file

---
+++ src/static_files/src_index_ts.rs
@@ -0,0 +1,12 @@
+pub const SRC_INDEX_TS: &str =
+r#"// Built by 🌊 Creta 
+//
+// You have to run the command below to start the app:
+//
+// yarn start
+//
+// Let's start editing code!
+
+let message: string = "Hello by Creta!"
+console.log(message)
+"#;
