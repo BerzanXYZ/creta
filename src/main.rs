ea47aa81b6a37271ce147efafa329d180f09217c
Berzan <berzan@tutanota.com>
2022-06-22 18:38:11 +0300

    Make init command work

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,53 @@ mod static_files;
 fn main() {
     // Declare project_name variable
     let mut project_name = String::new();
+    let mut project_folder = String::new();
+
 
     println!("🌊 Creta project builder");
 
     let args = utils::Args::new();
     if args.len() < 2 {
-        println!("What is the name of your project: ");
-        if let Err(_) = std::io::stdin().read_line(&mut project_name) {
-            println!("Cannot read your input.");
-            return;
+        while project_name.len() < 2 {
+            println!("What is the name of your project: ");
+            if let Err(_) = std::io::stdin().read_line(&mut project_name) {
+                println!("Cannot read your input");
+                return;
+            }
+            project_name.pop();
+            if project_name == "" {
+                println!("Cannot set an empty name");
+                println!();
+            }
         }
+        // Set project folder
+        project_folder = format!("/{}", project_name)
     } else {
-        project_name = match args.get(1) {
-            Some(s) => match s {
-                    "init" => match utils::get_work_folder() {
-                        Some(d) => d,
-                        None => {
-                            println!("");
-                            return ;
-                        } 
-                    },
-                    _ => {
-                        s.to_string()
-                    }
-            },
+        // Parse command
+        let command = match args.get(1) {
+            Some("init") => "init",
+            Some(p_name) => p_name,
             None => {
-                println!("Cannot build your project");
-                return ;
-            },
+                println!("Cannot build you project!");
+                return
+            }
+        };
+        // If command is init
+        if command == "init" {
+            project_name = match utils::get_work_folder() {
+                Some(d) => d,
+                None => {
+                    println!("Cannot build your project!");
+                    return
+                }
+            }
+            // Leave project folder empty to use current work folder
+        } else {
+            // If the command isn't init, command is the name of the project
+            project_name = command.to_string();
+            project_folder = format!("/{}", project_name);
         }
+
     }
 
 
@@ -44,30 +62,30 @@ fn main() {
     };
 
     // Define project_directory
-    let project_dir = format!("{}/{}/",work_dir, project_name);
+    let path_to_project = format!("{}{}/",work_dir, project_folder);
 
 
     // Create the main folder and src folder
     // If an error occurs stop the app
-    match std::fs::create_dir_all(format!("{}src", project_dir)) {
+    match std::fs::create_dir_all(format!("{}src", path_to_project)) {
         Ok(_) => (),
         Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
     }
     
     // Create and write the files
-    match std::fs::write(format!("{}package.json", project_dir), static_files::PACKAGE_JSON.replace("PROJECT_NAME", &project_name)) {
+    match std::fs::write(format!("{}package.json", path_to_project), static_files::PACKAGE_JSON.replace("PROJECT_NAME", &project_name)) {
         Ok(_) => (),
         Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
     }
-    match std::fs::write(format!("{}tsconfig.json", project_dir), static_files::TSCONFIG_JSON) {
+    match std::fs::write(format!("{}tsconfig.json", path_to_project), static_files::TSCONFIG_JSON) {
         Ok(_) => (),
         Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
     }
-    match std::fs::write(format!("{}.gitignore", project_dir), static_files::_GITIGNORE) {
+    match std::fs::write(format!("{}.gitignore", path_to_project), static_files::_GITIGNORE) {
         Ok(_) => (),
         Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
     }
-    match std::fs::write(format!("{}src/index.ts", project_dir), static_files::SRC_INDEX_TS) {
+    match std::fs::write(format!("{}src/index.ts", path_to_project), static_files::SRC_INDEX_TS) {
         Ok(_) => (),
         Err(_) => {println!("🌊 An error occured while creating the app..."); return;},
     }
