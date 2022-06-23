6d5ca62c127294bad882b4d4c028996d4db81454
Berzan <berzan@tutanota.com>
2022-06-23 15:16:02 +0300

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -2,34 +2,39 @@ mod utils;
 mod static_files;
 
 fn main() {
-    // Declare project_name variable
     let mut project_name = String::new();
     let mut command = String::new();
 
     println!("🌊 Creta app builder");
 
     let args = utils::Args::new();
+    // If no command is given;
     if args.len() < 2 {
+        // While project_name is empty
         while project_name.len() < 1 {
             println!("What is the name of your project: ");
+            // Read user input and set project_name
             if let Err(_) = std::io::stdin().read_line(&mut project_name) {
                 println!("Cannot read your input");
                 return;
             }
+            // Delete newline character
             project_name.pop();
+            // If project name is empty, tell user
             if project_name == "" {
                 println!("Cannot set an empty name");
                 println!();
             }
         }
-    } else {
         // If a command name is given, do these
+    } else {
         command = match args.get(1) {
-            // If command is init, set project name as current folder's name
+            // If the command is init, set project name as current folder's name
             Some("init") => {
                 project_name = utils::get_work_folder().unwrap();
                 "init".to_string()
             },
+            // If the command represents project name, set it
             Some(p_name) => {
                 project_name = p_name.to_string();
                 "".to_string()
@@ -72,8 +77,10 @@ fn main() {
 
     println!();
     println!("To start your app:");
+    // If current directory is in the project's base folder
+    // User doesn't have to use "cd" command
     if command != "init" {
-        println!("  cd {}", project_name);
+        println!("   cd {}", project_name);
     }
     println!("   yarn");
     println!("   yarn start");
